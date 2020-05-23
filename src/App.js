2c6524c736046b3854440d577640b081b20bbd37
Asterisk <98jiminlee@gmail.com>
2020-05-23 12:18:18 +0900

    1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//props
+//<App name={"홍길동"} sex={"남자"} age={"30"}/>
+//function에서는 this를 사용하지 않습니다. class에서만 존재
+function App(props) {
+    const msg=props.msg;
+    const data=msg.split("\n"); //[React, Redux...] [{},{}]
+    const print = data.map((subject)=>
+        <li>{subject}</li>
+    )
+  return (
+      <div>
+        {/*<h1>이름:{props.name}</h1>
+        <h1>성별:{props.sex}</h1>
+        <h1>나이:{props.age}</h1>*/}
+        <ul>
+            {print}
+        </ul>
+        {/*<h1>{props.msg}</h1>*/}
+      </div>
+  );
+}
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,124 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+/*
+*   const [변수명, 메소드] = useState("[]") ("{}") ("0") (false)
+*                   ======= setter
+*         [music, setMusic]
+*
+*   useEffect(()=>{
+*       처리 => 데이터 읽기    axios, fetch...
+*   });
+*
+*   componentDidMount
+*   componentDidUpdate
+*   ========================
+*
+*   useEffect(()=>{
+*       처리 => 데이터 읽기     axios, fetch...
+*   },[]);
+*
+* */
+/*
+*   class App extends Component{
+*       constructor(){
+*           this.state={
+*               music:[]    ===>const [music, setMusic] = useState([]);
+*           }
+*       }
+*       componentDidMount(){
+*           this.setState({music:res.data})
+*       }
+*       useEffect(()=>{
+*
+*       }
+*       render(){
+*           html
+*       }
+*   }
+* */
+function App2(){
+
+    //let music=[];
+    const [music, setMusic]=useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:3000/music.json')
+            .then((res)=>{
+                //music=res.data;
+                setMusic(res.data);
+                console.log(res.data);
+            })
+    },[])
+    // console.log(music)
+    // render()
+    // for if
+    const html = music.map((m)=>
+        <tr>
+            <td>{m.rank}</td>
+            <td>
+                {/*다중 if 문*/}
+                {
+                    m.state==="상승" &&
+                        <span style={{"color":"red"}}>▲{m.idcrement}</span>
+                }
+                {
+                    m.state==="하강" &&
+                    <span style={{"color":"blue"}}>▼{m.idcrement}</span>
+                }
+                {
+                    m.state==="유지" &&
+                    <span style={{"color":"grey"}}>-</span>
+                }
+            </td>
+            <td><img src={m.poster} width={"35"} height={"35"}/></td>
+            <td>{m.title}</td>
+            <td>{m.singer}</td>
+        </tr>
+    )
+    return (
+        <div className={"row"}>
+                <H/>
+                <div style={{"height":"30px"}}></div>
+                <table className={"table"}>
+                    <tr>
+                        <td>
+                            <input type={"text"} className={"input-sm"} size={"25"}/>
+                        </td>
+                    </tr>
+                </table>
+            <table className={"table"}>
+                <thead>
+                    <tr className={"success"}>
+                        <th>순위</th>
+                        <th>등폭</th>
+                        <th></th>
+                        <th>노래명</th>
+                        <th>가수명</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {html}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+// 호출시 <H>
+/*function H(){
+    return(
+        <h1 className={"text-center"}>Music Top50</h1>
+    )
+}*/
+const H=()=>{
+    const color=["#d2d2ff","#a0a0ff","pink","yellow","blue"];
+    const no = parseInt(Math.random()*5);//random(0.0~0.99)
+    return(
+        <h1 className={"text-center"} style={{"color":color[no]}}>Music Top50</h1>
+    )
+}
+/*const H=function(){
+    return(
+        <h1 className={"text-center"}>Music Top50</h1>
+    )
+}*/
+export default App2;
